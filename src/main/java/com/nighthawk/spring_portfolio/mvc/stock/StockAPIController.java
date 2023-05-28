d4050ea114b563bdce79f8a47bd16350b36303ce
JayManjrekar <jaymanjrekarapwork@gmail.com>
2023-05-27 23:42:40 -0700

    added create and update methods

--- src/main/java/com/nighthawk/spring_portfolio/mvc/stock/StockAPIController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/stock/StockAPIController.java
@@ -23,5 +23,24 @@ public class StockAPIController {
         return ResponseEntity.ok(stockService.getAllStocks());
     }
 
+    // Get a Single Stock
+    @GetMapping("/{id}")
+    public ResponseEntity<Stock> getStockById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(stockService.getStockById(id));
+    }
 
+    // Create a new Stock
+    @PostMapping
+    public ResponseEntity<Stock> createStock(@RequestBody Stock stock) {
+        return ResponseEntity.ok(stockService.createStock(stock));
+    }
+
+    // Update a Stock
+    @PutMapping("/{id}")
+    public ResponseEntity<Stock> updateStock(@PathVariable("id") Long id, @RequestBody Stock stock) {
+        return ResponseEntity.ok(stockService.updateStock(id, stock));
+    }
+
+
+    }
 }
