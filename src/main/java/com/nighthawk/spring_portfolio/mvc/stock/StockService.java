c49051749bf3ad542503ee39e516c31440aae7ac
JayManjrekar <jaymanjrekarapwork@gmail.com>
2023-05-27 23:47:25 -0700

    added StockService class and made a few methods

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/stock/StockService.java
@@ -0,0 +1,26 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Stock;
+import com.example.demo.repository.StockRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StockService {
+
+    private final StockRepository stockRepository;
+
+    public StockService(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
+
+    public List<Stock> getAllStocks() {
+        return stockRepository.findAll();
+    }
+
+    public Stock getStockById(Long id) {
+        return stockRepository.findById(id).orElseThrow(() -> new RuntimeException("Stock not found"));
+    }
+
+}
