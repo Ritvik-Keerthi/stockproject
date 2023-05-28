2eca0a27b520490c22bb550d39655f0444fccd89
JayManjrekar <jaymanjrekarapwork@gmail.com>
2023-05-27 23:46:28 -0700

    added price method

--- src/main/java/com/nighthawk/spring_portfolio/mvc/stock/StockMethds.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/stock/StockMethds.java
@@ -38,5 +38,15 @@ public class Stock {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
     }
 }
