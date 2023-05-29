31cae03df5d6c315fd29a64b12178e89bcd974df
JayManjrekar <jaymanjrekarapwork@gmail.com>
2023-05-28 21:18:24 -0700

    updated jpa repository

--- src/main/java/com/nighthawk/spring_portfolio/mvc/stock/StockJpaRepository.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/stock/StockJpaRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 // JPA is an object-relational mapping (ORM) to persistent data, originally relational databases (SQL). Today JPA implementations has been extended for NoSQL.
 public interface StockJpaRepository extends JpaRepository<Stock, Long> {
     // JPA has many built in methods, these few have been prototyped for this application
-    void save(String Joke);
-    List<Stock> findAllByOrderByJokeAsc();
-    List<Stock> findByJokeIgnoreCase(String joke);
+
+
 }
